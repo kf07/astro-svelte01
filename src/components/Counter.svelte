e452a2007f6a48bafd370917e62729467da32dac
kango furusu <furusu0715@gmail.com>
2023-12-28 18:56:02 +0900

    test push

--- src/components/Counter.svelte
+++ src/components/Counter.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
-	let count = 0;
+	let count: number = 0;
 	const addCount = () => {
 		count++;
 	}
+	$: count1 = count + 5;
 </script>
 
 <p>{count}</p>
 <button on:click={addCount}>add</button>
+<p>{count1}</p>
+<p>deploy test</p>
