4494a629e066e37dc783f4f003fbdd4b3adbcbe8
kango furusu <furusu0715@gmail.com>
2024-02-06 18:44:29 +0900

    emptyテスト

--- src/components/Empty.svelte
+++ src/components/Empty.svelte
@@ -13,12 +13,21 @@
 <button on:click={showChild}>子要素表示</button>
 
 <style>
+	@keyframes -global-rotation-r {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(-360deg);
+		}
+	}
 	.wrap {
 		padding: 20px;
 		&:empty {
 			width: 20px;
 			height: 20px;
 			background-color: pink;
+			animation: rotation-r 4s linear infinite;
 		}
 	}
 </style>
