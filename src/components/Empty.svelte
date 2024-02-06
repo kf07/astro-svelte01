3c48ebc4cf3dbaa430fdb49f99fcc93f1311bd18
kango furusu <furusu0715@gmail.com>
2024-02-06 16:51:23 +0900

    emptyテスト

---
+++ src/components/Empty.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	let show: boolean = false;
+	const showChild = () => {
+		show = true;
+	}
+</script>
+
+<div class="wrap">
+	{#if show === true}
+		<p>子要素</p>
+	{/if}
+</div>
+<button on:click={showChild}>子要素表示</button>
+
+<style>
+	.wrap {
+		padding: 20px;
+		&:empty {
+			width: 20px;
+			height: 20px;
+			background-color: pink;
+		}
+	}
+</style>
